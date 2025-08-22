507031f9ea19f5dfd5d7665e5b0d8fab29acefaf
Marcus Caue <cauemarcuscauefb@gmail.com>
2025-08-22 10:31:24 -0300

    O sistema funciona :)

---
+++ README.md
@@ -0,0 +1,3 @@
+# metadadata-audio-changer
+
+Documentação da biblioteca `node-id3`: https://www.npmjs.com/package/node-id3
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,48 @@
-import { Arma } from "./classes/Arma.ts";
+import NodeID3 from "node-id3";
+import { writeFileSync, readFileSync } from "fs";
 
-const espadaDiamante: Arma = new Arma("Espada de Diamante", 10, 1);
-console.log(espadaDiamante); 
+const showAllMetadata = (fileName: string) => {
+  const filePath = `./files/${fileName}.mp3`;
+  const metadata = NodeID3.read(filePath);
+  console.log(metadata);
+}
+
+function escrita(fileName: string, newMetada: object) : boolean | Error {
+  const filePath = `./files/${fileName}.mp3`;
+  const result = NodeID3.update(newMetadata, filePath);
+  return result;
+}
+
+function extractImage(fileName: string) : boolean | Error {
+  const filePath = `./files/${fileName}.mp3`;
+  const { image } = NodeID3.read(filePath);
+
+  if (typeof image == "object") {
+    const imageFileName = `${fileName}.${ (image.mime == "image/jpeg") ? "jpg" : "undefined" }`
+    writeFileSync("./files/images/" + imageFileName, image.imageBuffer);
+    return true;
+  }
+
+  return false;
+}
+
+const imageBuffer = readFileSync("./files/images/01-elementos-basicos-da-moral-catolica-os-dez-mandamentos-escritos-no-coracao-humano.jpg")
+
+const newMetadata = {
+  title: "1º Mandamento Superstição, Adivinhação e Idolatria",
+  artist: "Padre Paulo Ricardo",
+  album: "Curso dos Dez Mandamentos",
+  image: {
+    mime: "image/jpeg",
+    type: {
+      id: 1,
+      name: "front cover"
+    },
+    description: "Moisés segurando as duas Tábuas da Lei",
+    imageBuffer: imageBuffer
+  }
+}
+
+showAllMetadata("07-1-mandamento-supersticao-adivinhacao-e-idolatria")
+const result = escrita("07-1-mandamento-supersticao-adivinhacao-e-idolatria", newMetadata)
+console.log(result);
